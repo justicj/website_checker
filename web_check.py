e8a037a1a45b227cd19b40e9ad1103799a731125
Jason Justice <jayjustice@gmail.com>
2021-12-23 17:40:52 -0800

    moved to ifttt

--- web_check.py
+++ web_check.py
@@ -1,56 +1,63 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import requests
 import difflib
 import json
 import time
 
-#variables
-url = "http://www.cityofvista.com/services/city-departments/recreation-comm-services/programs-services/softball/monday-men-s-b-c"
-div_id = "ColumnUserControl3"
-#testing stuff
-#url = "https://justicj.github.io/index.html"
-#div_id = 'crapola'
-pushbullet_token = "insertToken"
-pushbullet_subject = "Men's Monday Softball Website Change"
-pushbullet_body = "Something has changed on Vista Mens Monday Night Softball website, please check it out at http://www.cityofvista.com/services/city-departments/recreation-comm-services/programs-services/softball/monday-men-s-b-c"
-interval = 1800
+# variables
+url = "https://www.cityofvista.com/city-services/recreation-community-services/youth-basketball/girls-b"
+div_id = "widget_4_9319_5903"
+subject = "Girls B"
+body = "Something has changed on " + subject + " website, please check it out at " + url
+ifttt_webhook = "https://maker.ifttt.com/trigger/GirlsB/with/key/insertkeyhere"
+interval = 10
 
 def get_div(url):
     #requests module to get url content
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "lxml")
-    #div = soup.find('div', id=div_id)
-    div = soup.find('div', {'id':div_id})
-    return div.text
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "lxml")
+        #div = soup.find('div', id=div_id)
+        div = soup.find('div', {'id':div_id})
+        return div.text
+    except requests.exceptions.RequestException as e:
+        print("Exception is: ", e)
+        return("failed")
 
 def get_diff_ratio(base, compare):
     d = difflib.SequenceMatcher(None, base, compare)
     return d.ratio()
 
-def send_push(title, body, token):
-    data_send = {"type": "note", "title": title, "body": body}
-    ACCESS_TOKEN = token
-    resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send),
-                         headers={'Authorization': 'Bearer ' + ACCESS_TOKEN, 'Content-Type': 'application/json'})
-    if resp.status_code != 200:
-        raise Exception('Something wrong')
-    else:
-        print ("PushBullet Sent!")
+
+def webhook ():
+    try:
+        values = {"value1": subject, "value2": body}
+        print(values)
+        r = requests.post(url = ifttt_webhook, data = values)
+        return(r.status_code, r.text)
+    except requests.exceptions.RequestException as e:
+        return("failed", "e")
 
 def run_check(base):
     while True:
-        print("Sleeping for ", interval)
+        print("Sleeping for", interval)
         time.sleep(interval)
         print("Getting Latest Content")
         compare = get_div(url)
-        print("comparing base and latest content")
-        ratio = get_diff_ratio(base, compare)
-        if ratio < 1.0:
-            print ("Change : ", ratio, " ratio")
-            send_push(pushbullet_subject, pushbullet_body, pushbullet_token)
-            base = compare
-        if ratio == 1.0:
-            print ("Content matches")
+        if compare != "failed":
+            print("comparing base and latest content")
+            print(compare)
+            ratio = get_diff_ratio(base, compare)
+            print(ratio)
+            if ratio < 1.0:
+                print ("Change : ", ratio, " ratio")
+                code, text = webhook()
+                print(code, text)
+                # send_push(subject, body, pushbullet_token)
+                base = compare
+            if ratio == 1.0:
+                print ("Content matches")
 
 #get initial div content, set to base
 print("Getting Base Url Content")
